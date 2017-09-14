d8163c317759a29e7261002685008dcf2c774c29
William <wmcabangon@gmail.com>
2017-09-14 14:25:13 +0100

    skinny controllers

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,15 @@
 require_relative 'player'
 
 class Game
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
   def hit(player)
     player.receive_damage
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,14 @@
 require 'game'
 
 describe Game do
-  it 'uses game to hit other player' do
-    player2 = double
-    expect(player2).to receive(:receive_damage)
-    subject.hit(player2)
+let(:player_1) { double:player }
+let(:player_2) { double:player }
+subject(:game) { described_class.new(player_1, player_2) }
+
+  describe '#hit' do
+    it 'uses game to hit other player' do
+      expect(player_2).to receive(:receive_damage)
+      game.hit(player_2)
+    end
   end
 end
